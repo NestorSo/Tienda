6a8954d4912ed6326b32a5f1da9b72f5fdc84039
NestorSo <arist@DESKTOP-C9M81HQ>
2022-11-02 17:50:14 -0600

    XD

--- TiendaDeRopa/Inventario.cs
+++ TiendaDeRopa/Inventario.cs
@@ -2,9 +2,19 @@
 {
     public class Inventario
     {
+        private string color;
+        private int talla;
+        private string genero;
 
+        public Inventario(string color, int talla, string genero)
+        {
+            this.color = color;
+            this.talla = talla;
+            this.genero = genero;
+        }
 
-
-
+        public string Color { get => color; set => color = value; }
+        public int Talla { get => talla; set => talla = value; }
+        public string Genero { get => genero; set => genero = value; }
     }
 }
