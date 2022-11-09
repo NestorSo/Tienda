c248a1bde4c12512a4b77f6d19b11bcc174f5f0c
NestorSo <Nestorsoto2601@gmail.com>
2022-11-08 18:19:57 -0600

    NO se que pasó

--- TareaDeCurso/Login.Designer.cs
+++ TareaDeCurso/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TareaDeCurso
 {
-    partial class Login
+    partial class frmLogin
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtContraseña = new System.Windows.Forms.TextBox();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btoSalir = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(60, 104);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(152, 23);
+            this.txtUsuario.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Digite su usuario ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(380, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Inicio de Sesion ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(232, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Digite la contraseña del respectivo usuario ";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(60, 187);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(152, 23);
+            this.txtContraseña.TabIndex = 4;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(60, 288);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(88, 23);
+            this.btnInicio.TabIndex = 5;
+            this.btnInicio.Text = "Iniciar sesion ";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btoSalir
+            // 
+            this.btoSalir.Location = new System.Drawing.Point(205, 288);
+            this.btoSalir.Name = "btoSalir";
+            this.btoSalir.Size = new System.Drawing.Size(87, 23);
+            this.btoSalir.TabIndex = 6;
+            this.btoSalir.Text = "Salir ";
+            this.btoSalir.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TareaDeCurso.Properties.Resources.store_1338629_960_720;
+            this.pictureBox1.Location = new System.Drawing.Point(380, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(316, 207);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btoSalir);
+            this.Controls.Add(this.btnInicio);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUsuario);
+            this.Name = "frmLogin";
+            this.Text = "Tienda de Ropa(Inicio de sesion)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtUsuario;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtContraseña;
+        private Button btnInicio;
+        private Button btoSalir;
+        private PictureBox pictureBox1;
     }
 }
--- TareaDeCurso/Login.cs
+++ TareaDeCurso/Login.cs
@@ -1,10 +1,39 @@
 namespace TareaDeCurso
 {
-    public partial class Login : Form
+    public partial class frmLogin : Form
     {
-        public Login()
+        public frmLogin()
         {
             InitializeComponent();
+            Usuarios();
+        }
+       Dictionary<string, string> usuarios = new Dictionary<string, string>();
+        frmTiendaDeRopa Tienda = new frmTiendaDeRopa(); 
+        public void Usuarios()
+        {
+            usuarios.Add("Nsoto26007","N2601");
+            usuarios.Add("Cpalaios26007","C2601");
+            usuarios.Add("Dchavarria26007","D2601");
+        }
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            string user = txtUsuario.Text;
+            string pass= txtContraseña.Text;
+            int i = 5;
+            if (usuarios.ContainsKey(user) && usuarios.ContainsValue(pass))
+            {
+                Tienda.ShowDialog();
+            }
+            else
+            {
+                
+                    txtContraseña.Clear();
+                    txtUsuario.Clear();
+                    MessageBox.Show("El usuario ingresado o la contraseña son incorretos vuelva a ingresarlos",
+                        "le quedan " + i-- + " intentos", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+                
+            }
         }
     }
 }
